3b2cb4e7dec0f4c273795b14cb32c95f9f6a65d0
Julien <julien.falgayrettes@gmail.com>
2021-12-21 21:29:52 +0100

    mise en commun

---
+++ build/tools/CMakeFiles/tools.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tools.dir/format/mesh_io_obj.cpp.o"
+  "CMakeFiles/tools.dir/format/mesh_io_obj.cpp.o.d"
+  "CMakeFiles/tools.dir/format/mesh_io_off.cpp.o"
+  "CMakeFiles/tools.dir/format/mesh_io_off.cpp.o.d"
+  "CMakeFiles/tools.dir/glhelper.cpp.o"
+  "CMakeFiles/tools.dir/glhelper.cpp.o.d"
+  "CMakeFiles/tools.dir/mat4.cpp.o"
+  "CMakeFiles/tools.dir/mat4.cpp.o.d"
+  "CMakeFiles/tools.dir/mesh.cpp.o"
+  "CMakeFiles/tools.dir/mesh.cpp.o.d"
+  "CMakeFiles/tools.dir/tga.cpp.o"
+  "CMakeFiles/tools.dir/tga.cpp.o.d"
+  "CMakeFiles/tools.dir/triangle_index.cpp.o"
+  "CMakeFiles/tools.dir/triangle_index.cpp.o.d"
+  "CMakeFiles/tools.dir/vec2.cpp.o"
+  "CMakeFiles/tools.dir/vec2.cpp.o.d"
+  "CMakeFiles/tools.dir/vec3.cpp.o"
+  "CMakeFiles/tools.dir/vec3.cpp.o.d"
+  "CMakeFiles/tools.dir/vertex_opengl.cpp.o"
+  "CMakeFiles/tools.dir/vertex_opengl.cpp.o.d"
+  "libtools.a"
+  "libtools.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tools.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
